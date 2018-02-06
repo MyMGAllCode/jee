a4f84b3119daefb88259133a1ea1a33b016aae63
dktech89 <35030016+dktech89@users.noreply.github.com>
2018-02-07 00:04:39 +0530

    Add files via upload
    mail

---
+++ JavaMailAPI.java
@@ -0,0 +1,96 @@
+package com.test;
+
+
+	import java.util.Properties;  
+	import javax.mail.*;  
+	import javax.mail.internet.*;  
+	public class JavaMailAPI {
+	 public static void main(String[] args) {  
+	  
+	  String host="smtp.gmail.com";  
+	  final String user="dktech89@gmail.com";//change accordingly  
+	  //final String password="gmaildkpeng1989";//change accordingly  
+	    
+	  String to="dharmendrakp89@gmail.com";//change accordingly  
+	  
+	   /*//Get the session object  
+	   Properties props = new Properties();  
+	   props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+		props.put("mail.smtp.port", "587"); //TLS Port
+		props.put("mail.smtp.auth", "true"); //enable authentication
+		props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
+		
+	  // props.put("mail.smtp.host",host);  
+	 //  props.put("mail.smtp.auth", "true");
+	   props.put("mail.defaultEncoding", "UTF-8"); 
+	  // props.put(" mail.smtp.starttls.required", "true");
+	  // props.put("mail.smtp.starttls.enable", "true");
+	  // props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+	  // props.put("mail.smtp.socketFactory.fallback", "false");
+	  // props.put("mail.smtp.port", "465");
+	  // props.put("mail.smtp.socketFactory.port", "465");
+	     
+	   Session session = Session.getDefaultInstance(props,  
+	    new javax.mail.Authenticator() {  
+	      protected PasswordAuthentication getPasswordAuthentication() {  
+	    return new PasswordAuthentication(user,password);  
+	      }  
+	    });  
+	  
+	   //Compose the message  
+	    try {  
+	     MimeMessage message = new MimeMessage(session);  
+	     message.setFrom(new InternetAddress(user));  
+	     message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));  
+	     message.setSubject("Test");  
+	     message.setText("This is simple program of sending email using JavaMail API");  
+	       
+	    //send the message  
+	     Transport.send(message);  
+	  
+	     System.out.println("message sent successfully...");  
+	   
+	     } catch (MessagingException e) {e.printStackTrace();}  
+	 }  */
+	  
+	  final String username = "dharmendrakp89@gmail.com";
+		final String password = "gmailengdkp1989";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		//props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		//props.put("mail.smtp.port", "465");
+		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");;
+	  
+	  Session session = Session.getInstance(props,
+			  new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			  });
+
+			try {
+
+				Message message = new MimeMessage(session);
+				message.setFrom(new InternetAddress("dharmendrakp89@gmail.com"));
+				message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse("dharmendrakp89@gmail.com"));
+				message.setSubject("Testing Subject");
+				message.setText("Dear Mail Crawler,"
+					+ "\n\n No spam to my email, please!");
+
+				Transport.send(message);
+
+				System.out.println("Done");
+
+			} catch (MessagingException e) {
+				throw new RuntimeException(e);
+			}
+	 }
+	}
+	
+
+
