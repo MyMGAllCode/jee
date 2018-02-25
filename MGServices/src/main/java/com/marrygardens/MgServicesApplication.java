63317c7e06888d6a12ab617131c49aab39189e11
dktech89 <35030016+dktech89@users.noreply.github.com>
2018-02-25 08:52:09 +0530

    Add files via upload
    MGServices

---
+++ MGServices/src/main/java/com/marrygardens/MgServicesApplication.java
@@ -0,0 +1,12 @@
+package com.marrygardens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgServicesApplication.class, args);
+	}
+}
